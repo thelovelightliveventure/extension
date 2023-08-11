86af3d67244304e7ab6dcd780da4580fa27b5c33
Joyce T <134914215+thelovelightliveventure@users.noreply.github.com>
2023-08-11 15:13:34 +0545

    Update background.js

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ chrome.action.onClicked.addListener(async (tab) => {
     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
@@ -36,5 +36,5 @@ chrome.action.onClicked.addListener(async (tab) => {
     };
   } else if (nextState === "OFF") {
     location.reload;
-  }
+  };
 });
